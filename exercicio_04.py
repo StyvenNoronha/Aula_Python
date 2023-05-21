061da9037d2fb295eea996b61091e927bf92adea
Styven Noronha <styvenn16@gmail.com>
2023-05-21 12:31:35 -0300

    exercicio 3

---
+++ exercicio_04.py
@@ -0,0 +1,9 @@
+a = int(input('Digite  um valor '))
+b = int(input('Digite  outro  valor '))
+
+if a > b :
+   print(f'{a} e maior que o {b}')
+elif a < b:
+    print(f' o valor {b} e maior que o {a}')
+else:
+  print(' os dois valores sao iguais ')       
