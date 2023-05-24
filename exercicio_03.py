612269b053a55237f2b5f52e61d2c988a2df91f6
Styven Noronha <styvenn16@gmail.com>
2023-05-24 07:50:21 -0300

    exercicio 3

---
+++ exercicio_03.py
@@ -0,0 +1,40 @@
+# Exercicio 1
+'''
+try:
+
+  num = int(input('digite um numero '))
+
+  resto = num % 2 
+  if resto == 0:
+    print('esse numero e par')
+  else:
+    print('esse numero e impar')  
+
+
+except:
+  print('isso nao é um numero ')
+
+
+#Exercicio 2
+hora = int(input('Digite a hora.: EX 11, 13  ... '))
+
+if hora <= 11:
+  print('Bom dia')
+elif hora >= 12 and hora <= 17:
+  print('Boa tarde')
+else:
+  print('Boa noite')  
+
+#Exercicio 3
+nome = input('Digite seu primeiro nome ')
+
+ver = len(nome)
+
+if ver <= 4:
+  print('Seu nome e cuto')
+elif ver >= 5 and ver <=6:
+  print('Seu nome e normal')
+else:
+  print('Seu nome e grande ')    
+
+'''
